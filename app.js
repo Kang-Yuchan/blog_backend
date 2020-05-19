0309e0574b1fa82d4dc19e7344b3a99a69fcb5cc
Yuchan <mikuplayer123@gmail.com>
2020-05-20 08:24:41 +0900

    Upload th image file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const Post = require("./api/models/posts");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const postData = new Post();
 
 app.use((req, res, next) => {
@@ -22,6 +24,18 @@ app.get("/api/posts/:post_id", (req, res) => {
   } else {
     res.status(404).send("Not Found!");
   }
+});
+
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+  const newPost = {
+    id: `${Date.now()}`,
+    title: req.body.title,
+    content: req.body.content,
+    post_image: req.body["post-image"],
+    added_date: `${Date.now()}`,
+  };
+  postData.add(newPost);
+  res.status(201).send("Ok!!");
 });
 
 app.listen(4000, () => console.log(`Listening on: http://localhost:4000`));
